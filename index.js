e46d31b689e3e966b4f617e5cd7ae54e31403858
Jaykishan <jaykishannoida@gmail.com>
2022-07-06 11:33:21 +0530

    express add

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log('git');
-console.log('Hello')
+const express = require('express');
+const app =express();
+const port =8000;
+
+app.listen(port,function(err){
+    if(err){
+        console.log('err',err)
+        console.log(`error in euuning the server :${err}`)
+    }
+    console.log(`server is running in the port  :${port}`)
+})
